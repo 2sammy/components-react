617068e3bd6921cc5e0eb782274bb6a75769f383
2sammy <samuelmanoah61@gmail.com>
2022-08-19 18:53:56 +0300

    react course

---
+++ Navbar.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Navbar(){
+    return(
+        <nav>
+            <img src="../images/react-con-small.png" />
+            <h3> React Facts</h3>
+            <h4> React Course - project 1</h4>
+        </nav>
+    )
+}
