afe55486431acc135b7577e3b54efc7b6782e516
2sammy <samuelmanoah61@gmail.com>
2022-08-20 23:04:54 +0300

    react

--- Main.js
+++ Main.js
@@ -2,6 +2,15 @@ import React from "react"
 
 export default function Main(){
     return(
-        <hi>Main component</hi>
+    <main>
+        <hi className="main--title">Fun facts about React</hi>
+        <ul className="main--facts">
+            <li>Was first realised in 2003</li>
+            <li>Was originally created by jonah</li>
+            <li>Has well over 100k views</li>
+            <li>Is maintained by facebook</li>
+        </ul>
+
+    </main>
     )
 }
